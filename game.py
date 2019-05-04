0c665ee9d19c6272e5a8905b72675df4ea10a245
gammaflauge <mddevine@gmail.com>
2019-05-04 10:33:13 -0400

    :sparkles: Allow variable player count
    Update game to take in list of player names.
    Update game functions to account for player count between 1 and 4

--- game.py
+++ game.py
@@ -5,10 +5,8 @@ from player import Player
 
 
 class Game(object):
-    def __init__(self, player1, player2):
+    def __init__(self, player_names):
         self.hand_number = 0
-        self.player1 = player1
-        self.player2 = player2
         self.cut_card = None
         self.crib = []
 
@@ -17,27 +15,46 @@ class Game(object):
             for rank in range(1, 14):
                 self.deck.append(Card(rank, suit))
 
+        if isinstance(player_names, list):
+            self.players = [Player(name) for name in player_names]
+        else:
+            self.players = [Player(player_names)]
+
+        if len(self.players) <= 2:
+            self.cards_per_player = 6
+        else:
+            self.cards_per_player = 5
+
     def __repr__(self):
+        player_lines = ''
+        for player in self.players:
+            player_lines += f"  { player }\n"
+
         return (
             f"Hand Number: { self.hand_number }\n"
-            f"  { self.player1 }\n"
-            f"  { self.player2 }\n"
+            f"{ player_lines }"
             f"  cut card -- { self.cut_card }\n"
             f"  crib -- { self.crib }"
         )
 
     def deal(self):
+        self.crib = []
         random.shuffle(self.deck)
-        self.player1.hand = self.deck[:6]
-        self.player2.hand = self.deck[6:12]
-        self.cut_card = self.deck[12]
         self.hand_number = self.hand_number + 1
+        for pnum, player in enumerate(self.players):
+            player.hand = self.deck[pnum * self.cards_per_player:pnum *
+                                    self.cards_per_player + self.cards_per_player]
+        self.cut_card = self.deck[-1]
 
     def throw_to_crib(self):
-        self.crib = self.player1.hand[4:] + self.player2.hand[4:]
-        self.player1.hand = self.player1.hand[:4]
-        self.player2.hand = self.player2.hand[:4]
+        for player in self.players:
+            self.crib += player.hand[4:]
+            player.hand = player.hand[:4]
+        if len(self.crib) == 2:
+            self.crib += self.deck[-3:-1]  # -1 is the cut card
+        elif len(self.crib) == 3:
+            self.crib += self.deck[-2:-1]  # -1 is the cut card
 
     def score(self):
-        self.player1.score += Card.score_hand(self.player1.hand + [self.cut_card])
-        self.player2.score += Card.score_hand(self.player2.hand + [self.cut_card])
+        for player in self.players:
+            player.score += Card.score_hand(player.hand + [self.cut_card])
--- tests/test_game.py
+++ tests/test_game.py
@@ -1,9 +1,80 @@
 import unittest
+import random
 
 from game import Game
+from card import Card
 from player import Player
 
 
 class TestGame(unittest.TestCase):
     def setUp(self):
-        self.my_game = Game("p1", "p2")
+        p1 = "char"
+        p2 = "sam"
+        p3 = "zee"
+        p4 = "daisy"
+
+        self.game_1p = Game(p1)
+        self.game_2p = Game([p1, p2])
+        self.game_3p = Game([p1, p2, p3])
+        self.game_4p = Game([p1, p2, p3, p4])
+        self.all_games = [self.game_1p,
+                          self.game_2p, self.game_3p, self.game_4p]
+
+    def test_init_deck(self):
+        for game in self.all_games:
+            self.assertEqual(len(game.deck), 52)
+            for suit in ["C", "H", "S", "D"]:
+                card_count = len(
+                    [card for card in game.deck if card.suit == suit])
+                self.assertEqual(card_count, 13)
+            for rank in range(1, 14):
+                card_count = len(
+                    [card for card in game.deck if card.rank == rank])
+                self.assertEqual(card_count, 4)
+
+    def test_init_cards_per_player(self):
+        self.assertEqual(self.game_1p.cards_per_player, 6)
+        self.assertEqual(self.game_2p.cards_per_player, 6)
+        self.assertEqual(self.game_3p.cards_per_player, 5)
+        self.assertEqual(self.game_4p.cards_per_player, 5)
+
+    def test_deal(self):
+        for game in self.all_games:
+            self.assertIsNone(game.cut_card)
+            for player in game.players:
+                self.assertEqual(len(player.hand), 0)
+            game.deal()
+            for player in game.players:
+                self.assertEqual(len(player.hand), game.cards_per_player)
+            self.assertIsNotNone(game.cut_card)
+            self.assertIsInstance(game.cut_card, Card)
+
+    def test_throw_to_crib(self):
+        for game in self.all_games:
+            for _ in range(0, 5):
+                game.deal()
+                game.throw_to_crib()
+                for player in game.players:
+                    self.assertEqual(len(player.hand), 4)
+                self.assertEqual(len(game.crib), 4)
+
+    def test_score(self):
+        for game in self.all_games:
+            random.seed(512019)
+            game.deal()
+            game.throw_to_crib()
+            game.score()
+
+        self.assertEqual(self.game_4p.players[0].score, 4)
+        self.assertEqual(self.game_4p.players[1].score, 10)
+        self.assertEqual(self.game_4p.players[2].score, 2)
+        self.assertEqual(self.game_4p.players[3].score, 9)
+
+        self.game_4p.deal()
+        self.game_4p.throw_to_crib()
+        self.game_4p.score()
+
+        self.assertEqual(self.game_4p.players[0].score, 10)
+        self.assertEqual(self.game_4p.players[1].score, 16)
+        self.assertEqual(self.game_4p.players[2].score, 7)
+        self.assertEqual(self.game_4p.players[3].score, 11)
